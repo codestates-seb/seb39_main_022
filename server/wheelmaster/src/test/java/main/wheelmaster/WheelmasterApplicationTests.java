4f07f8c010d91ebf316ee4c23b316ec5a3402c22
yongho <yhrewind2022@gmail.com>
2022-09-14 14:49:13 +0900

    spring initialization

---
+++ server/wheelmaster/src/test/java/main/wheelmaster/WheelmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package main.wheelmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheelmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
