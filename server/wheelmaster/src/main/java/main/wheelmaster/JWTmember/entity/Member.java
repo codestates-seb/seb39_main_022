3fe6e5b95493ac826faa2c008d9481497cd64e4d
Dong Keun LIM <86710265+Limdongkeun@users.noreply.github.com>
2022-09-15 17:28:55 +0900

    jwtTest

--- server/wheelmaster/src/main/java/main/wheelmaster/JWTmember/entity/Member.java
+++ server/wheelmaster/src/main/java/main/wheelmaster/JWTmember/entity/Member.java
@@ -1,35 +1,35 @@
-//package main.wheelmaster.JWTmember.entity;
-//
-//
-//import com.fasterxml.jackson.annotation.JsonIgnore;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//
-//import javax.persistence.*;
-//import java.time.LocalDateTime;
-//
-//@NoArgsConstructor
-//@Getter
-//@Setter
-//@Entity
-//public class Member {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private long memberId;
-//
-//    @Column(unique = true, length = 200)
-//    private String email;
-//
-//    @JsonIgnore
-//    private String password;
-//
-//    private String nickName;
-//
-//    @Column(length = 50)
-//    private String tel;
-//
-//    private LocalDateTime createdAt = LocalDateTime.now();
-//    private LocalDateTime modifiedAt = LocalDateTime.now();
-//}
+package main.wheelmaster.JWTmember.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long memberId;
+
+    @Column(unique = true, length = 200)
+    private String email;
+
+    @JsonIgnore
+    private String password;
+
+    private String nickName;
+
+    @Column(length = 50)
+    private String tel;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+    private LocalDateTime modifiedAt = LocalDateTime.now();
+}
