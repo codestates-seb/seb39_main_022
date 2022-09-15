3a6b1a20bdb819067147d747ae58b2d7f6123ba1
Dong Keun LIM <86710265+Limdongkeun@users.noreply.github.com>
2022-09-15 23:58:44 +0900

    Jwt Member Controller

--- server/wheelmaster/src/main/java/main/wheelmaster/token/entity/Token.java
+++ server/wheelmaster/src/main/java/main/wheelmaster/token/entity/Token.java
@@ -4,6 +4,8 @@ package main.wheelmaster.token.entity;
 import lombok.*;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @NoArgsConstructor
@@ -15,7 +17,9 @@ import javax.persistence.Id;
 public class Token {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long tokenId;
+
     private String grantType;
     private String accessToken;
     private String refreshToken;
