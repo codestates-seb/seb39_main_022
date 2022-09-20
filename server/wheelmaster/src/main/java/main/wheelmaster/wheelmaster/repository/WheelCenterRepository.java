27961df65525340f87db35349e540c6c760f61b9
Dong Keun LIM <86710265+Limdongkeun@users.noreply.github.com>
2022-09-20 15:03:46 +0900

    Comment Controller

--- server/wheelmaster/src/main/java/main/wheelmaster/wheelmaster/repository/WheelCenterRepository.java
+++
@@ -1,9 +0,0 @@
-package main.wheelmaster.wheelmaster.repository;
-
-import main.wheelmaster.wheelmaster.entity.WheelCenter;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WheelCenterRepository extends JpaRepository<WheelCenter, Long> {
-}
