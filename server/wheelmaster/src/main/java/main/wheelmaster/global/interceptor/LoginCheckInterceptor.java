5546126ed87889b99be9639c2f2c43f65eb6795e
yongho <yhrewind2022@gmail.com>
2022-09-28 11:44:36 +0900

    intercept problem resolved

--- server/wheelmaster/src/main/java/main/wheelmaster/global/interceptor/LoginCheckInterceptor.java
+++ server/wheelmaster/src/main/java/main/wheelmaster/global/interceptor/LoginCheckInterceptor.java
@@ -3,6 +3,7 @@ package main.wheelmaster.global.interceptor;
 import lombok.extern.slf4j.Slf4j;
 import main.wheelmaster.member.SessionConst;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,10 +22,17 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
         if(session == null || session.getAttribute(SessionConst.LOGIN_MEMBER) == null) {
             log.info("미인증 사용자 요청");
 
-            response.sendRedirect("/login?redirectURL=" + requestURI);
+            response.sendRedirect("members/login?redirectURL=" + requestURI);
             return false;
        }
 
         return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        if(ex != null){
+            log.info("requestURI=[{}], Exception cause = [{}], message = [{}]", request.getRequestURI() ,ex.getCause(), ex.getMessage());
+        }
     }
 }
